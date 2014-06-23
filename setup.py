bc8411b0e1e881782662d9bf028dbba434b7d851
Kellen Fox <Kellen.Fox@wwu.edu>
2014-06-23 11:25:46 -0700

    A setup file so I can just install things

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='watdo',
+      version='0.1',
+      description='A stupid simple todo list',
+      author='Kellen Fox',
+      url='https://github.com/Kellel/watdo',
+      packages=['watdo'],)
